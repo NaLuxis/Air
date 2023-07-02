9f092df9e1a3b00bfd3fc0059b2080a04f0798f8
Yoann <yoannalves7@gmail.com>
2023-07-02 19:00:15 +0200

    ok

--- air10.py
+++ air10.py
@@ -1,19 +1,39 @@
 ############### Afficher le contenu ###############
 
+import sys
+
 ### Function ###
 
+def read_my_file(my_file: str) -> None:
+
+    try:
+        with open(my_file, "r") as f:
+            return f.read()
+        
+    except FileNotFoundError:
+        print(f"File : {my_file} don't exist")
+        exit()
+
+
+def incorrect_argument_count() -> None:
+
+    if len(sys.argv) != 2:
+        print("Error")
+        exit()
 
 
 ### Error ###
 
-
+incorrect_argument_count()
 
 ### Parsing ###
 
-
+my_file = sys.argv[1]
 
 ### Problem solving ###
 
-
+readed_file = read_my_file(my_file)
 
 ### Result ###
+
+print(readed_file)
